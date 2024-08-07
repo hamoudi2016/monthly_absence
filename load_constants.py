94548e95e090c108a39afec1177b5664909acac8
hamoudi2016 <n36112405@gmail.com>
2024-08-07 23:46:53 +0100

    Update load_constants.py

--- load_constants.py
+++ load_constants.py
@@ -1,21 +1,4 @@
 
-# import os
-# from datetime import date
-
-
-# # Define constants
-# absence_motif: list = list(["", "عطلة مرضية","عطلة أمومة"])
-# header_Title: list =list(["id", "Name", "CCP", "Grade", "Start Date","End Date", "Days", "Motif", "Category", "Month", "Anne"])
-# categorys: list = list(["الأساتذة","الأساتذة المتعاقدين","الإداريين","العمال المهنيين"])
-# months: list = list(["جانفي", "فيفري", "مارس", "أفريل", "ماي", "جوان","جويلية", "أوت", "سبتمبر", "اكتوبر", "نوفمبر", "ديسمبر"])
-# myfont: str = "ManaraDocs Amatti Font"  # Use a font that supports Arabic characters
-# ecole: str = 'متوسطة بزيط محمد'
-# city: str = 'أولاد جلال'
-
-# current_date: str = date.today().strftime("%d-%m-%Y")
-# excel_file = os.path.join(os.path.dirname(__file__), "mydata.xlsx")
-# columns_to_read: list = list(["name", "grade", "category","ccp"])
-
 import json
 
 def load_constants(file_path='constants.json'):
